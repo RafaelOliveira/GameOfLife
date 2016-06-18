ba35ec41c2d5efeba9133d362d5127203fb74696
Rafael Oliveira <rafah.so@gmail.com>
2016-06-18 02:43:03 -0300

    Initial commit

---
+++ Sources/Main.hx
@@ -0,0 +1,146 @@
+package;
+
+import kha.System;
+
+#if sys_windows
+import kha.Display;
+import kha.WindowOptions.Mode;
+#end
+
+#if (sys_html5 || sys_debug_html5)
+import kha.math.Vector2i;
+import js.Browser;
+import js.html.CanvasElement;
+#end
+
+class Main 
+{
+	public static var desktopWidth:Int;
+	public static var desktopHeight:Int;
+	
+	// size of the cell
+	public static var size:Int;
+	
+	// 1: g1, 2: g2
+	// 0: g1 with size 1 and g2 with size bigger than 1
+	public static var renderMethod:Int;
+	
+	public static var fps:Int;
+	
+	public static function main() 
+	{
+		setupParameters();
+		
+		#if (sys_windows || sys_linux || sys_osx)
+		desktopWidth = System.windowWidth();
+		desktopHeight = System.windowHeight();
+		#elseif (sys_html5 || sys_debug_html5)		
+		var size = setupCanvas();
+		desktopWidth = size.x;
+		desktopHeight = size.y;
+		#else
+		desktopWidth = System.windowWidth();
+		desktopHeight = System.windowHeight();
+		#end
+		
+		#if (sys_windows || sys_linux || sys_osx)
+		System.initEx('Game of Life', [{ width: desktopWidth, height: desktopHeight, mode: Mode.BorderlessWindow }], windowCallback, function() 		
+		{
+			new Project();
+		});
+		#else
+		System.init({ title: 'Game of Life', width: desktopWidth, height: desktopHeight }, function()
+		{
+			new Project();
+		});
+		#end		
+	}
+	
+	static function windowCallback(id:Int):Void {}
+	
+	static function setupParameters():Void
+	{
+		#if (sys_html5 || sys_debug_html5)
+		size = -1;
+		renderMethod = -1;
+		fps = -1;
+		
+		var data = Browser.location.search;
+		// remove the '?'
+		if (data.length > 0)
+			data = data.substr(1);
+		
+		var paramList = data.split('&');
+		if (paramList.length == 0 && data.length > 0)
+			paramList.push(data);
+		
+		for (param in paramList)
+		{
+			var vars = param.split('=');
+			if (vars[0] != null && vars[1] != null)
+			{
+				switch(vars[0])
+				{
+					case 'size':						
+						var test = Std.parseInt(vars[1]);
+						if (test != null && test > 0)
+							size = test;
+						else
+							size = 3;
+					
+					case 'render':
+						if (vars[1] == 'g1')
+							renderMethod = 1;
+						else if (vars[1] == 'g2')
+							renderMethod = 2;
+						else
+							renderMethod = 0;
+					
+					case 'fps':
+						var test = Std.parseInt(vars[1]);
+						if (test != null && test > 0)
+							fps = test;
+						else
+							fps = 60;
+				}
+			}
+		}
+		
+		if (size == -1)
+			size = 3;
+			
+		if (renderMethod == -1)
+			renderMethod = 0;
+			
+		if (fps == -1)
+			fps = 60;
+		
+		#else
+		size = 3;
+		renderMethod = 0;
+		fps = 60;
+		#end
+	}
+	
+	#if (sys_html5 || sys_debug_html5)	
+	static function setupCanvas():Vector2i
+	{
+		var body = Browser.document.body;
+		body.style.margin = '0';
+		body.style.padding = '0';
+		body.style.height = '100%';
+		body.style.overflow = 'hidden';
+		
+		var canvas:CanvasElement = cast Browser.document.getElementById('khanvas');
+		var w:Int = Browser.window.innerWidth;
+		var h:Int = Browser.window.innerHeight;
+		
+		canvas.style.width = '${w}px';
+		canvas.style.height = '${h}px';
+		canvas.width = w;
+		canvas.height = h;
+		
+		return new Vector2i(w, h);
+	}
+	#end
+}
---
+++ Sources/Project.hx
@@ -0,0 +1,198 @@
+// googledrive.com/host/0B9NwYQy46Xl5U0J6RHVfR0FYUmc 
+
+package;
+
+import kha.Color;
+import kha.Framebuffer;
+import kha.Scheduler;
+import kha.System;
+
+typedef Matrix = Array<Array<Bool>>;
+
+class Project 
+{	
+	var table1:Matrix;
+	var table2:Matrix;
+	var activeTable:Matrix;
+	var otherTable:Matrix;
+	var indexActive:Int;
+	
+	var cols:Int;
+	var lines:Int;
+	
+	var liveCells:Int;	
+	
+	public function new() 
+	{				
+		// choose the method using the size
+		if (Main.renderMethod == 0)
+		{
+			if (Main.size == 1)
+				System.notifyOnRender(renderWithG1);
+			else
+				System.notifyOnRender(renderWithG2);
+		}
+		// use g1 and force size = 1
+		else if (Main.renderMethod == 1)
+		{
+			Main.size = 1;
+			System.notifyOnRender(renderWithG1);
+		}
+		// use g2
+		else
+			System.notifyOnRender(renderWithG2);
+			
+		cols = Std.int(Main.desktopWidth / Main.size);
+		lines = Std.int(Main.desktopHeight / Main.size);
+		
+		setupTables();
+		createFirstGen();
+		
+		Scheduler.addTimeTask(update, 0, 1 / Main.fps);
+	}
+	
+	inline function setupTables():Void
+	{
+		table1 = new Matrix();
+		table2 = new Matrix();
+		
+		for (y in 0...lines)
+		{
+			table1.push(new Array<Bool>());
+			table2.push(new Array<Bool>());
+			
+			for (x in 0...cols)
+			{
+				table1[y].push(false);
+				table2[y].push(false);
+			}
+		}
+	}
+	
+	inline function createFirstGen():Void
+	{
+		activeTable = table1;
+		otherTable = table2;
+		indexActive = 1;
+		
+		for (y in 0...lines)
+		{
+			for (x in 0...cols)
+			{
+				if (Math.random() > 0.8)
+					activeTable[y][x] = true;
+			}
+		}		
+	}
+
+	function update(): Void 
+	{
+		for (y in 0...lines)
+		{
+			for (x in 0...cols)
+			{
+				otherTable[y][x] = false;
+				calculateLiveCells(x, y);
+				
+				// live cell
+				if (activeTable[y][x])
+				{
+					if (liveCells == 2 || liveCells == 3)
+						otherTable[y][x] = true;					
+				}
+				// dead cell
+				else
+				{
+					if (liveCells == 3)
+						otherTable[y][x] = true;
+				}				
+			}
+		}
+		
+		if (indexActive == 1)
+		{
+			activeTable = table2;
+			otherTable = table1;
+			indexActive = 2;
+		}
+		else
+		{
+			activeTable = table1;
+			otherTable = table2;
+			indexActive = 1;
+		}
+	}
+
+	// Calculate the total of live neighbors cell
+	function calculateLiveCells(x:Int, y:Int):Void
+	{
+		liveCells = 0;		
+		
+		// top
+		if (y != 0 && activeTable[y - 1][x])
+			liveCells++;
+			
+		// bottom
+		if (y != (lines - 1) && activeTable[y + 1][x])
+			liveCells++;
+			
+		// left
+		if (x != 0 && activeTable[y][x - 1])
+			liveCells++;
+			
+		// right
+		if (x != (cols - 1) && activeTable[y][x + 1])
+			liveCells++;
+			
+		// top left		
+		if (x != 0 && y != 0 && activeTable[y - 1][x - 1])
+			liveCells++;
+			
+		// top right
+		if (x != (cols - 1) && y != 0 && activeTable[y - 1][x + 1])
+			liveCells++;
+			
+		// bottom left		
+		if (x != 0 && y != (lines - 1) && activeTable[y + 1][x - 1])
+			liveCells++;
+			
+		// bottom right		
+		if (x != (cols - 1) && y != (lines - 1) && activeTable[y + 1][x + 1])
+			liveCells++;
+	}
+
+	function renderWithG2(framebuffer: Framebuffer): Void 
+	{
+		framebuffer.g2.begin();
+		
+		for (y in 0...lines)
+		{
+			for (x in 0...cols)
+			{
+				if (activeTable[y][x])
+					framebuffer.g2.fillRect(x * Main.size, y * Main.size, Main.size, Main.size);
+			}
+		}
+		
+		framebuffer.g2.end();
+	}
+	
+	function renderWithG1(framebuffer: Framebuffer): Void 
+	{
+		framebuffer.g2.begin();
+		framebuffer.g2.end();
+		
+		framebuffer.g1.begin();
+		
+		for (y in 0...lines)
+		{
+			for (x in 0...cols)
+			{
+				if (activeTable[y][x])
+					framebuffer.g1.setPixel(x, y, Color.White);
+			}
+		}
+		
+		framebuffer.g1.end();
+	}
+}
